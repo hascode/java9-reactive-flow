9401abd51c174cf68f32e073789ff0a1a1fbc290
Micha Kops <micha.kops@gmail.com>
2018-01-04 21:51:49 +0100

    News publisher example updated

--- src/com/hascode/tutorial/Main.java
+++ src/com/hascode/tutorial/Main.java
@@ -31,16 +31,15 @@ public class Main {
     }
   }
 
-  static class NewsPublisher extends SubmissionPublisher<News> {
-
-  }
-
   static class NewsSubscriber implements Flow.Subscriber<News> {
 
     private Subscription subscription;
+    private static final int MAX_NEWS = 3;
+    private int newsReceived = 0;
 
     @Override
     public void onSubscribe(Subscription subscription) {
+      System.out.printf("new subscription %s\n", subscription);
       this.subscription = subscription;
       subscription.request(1);
     }
@@ -48,6 +47,13 @@ public class Main {
     @Override
     public void onNext(News item) {
       System.out.printf("news received: %s (%s)\n", item.getHeadline(), item.getDate());
+      newsReceived++;
+      if (newsReceived >= MAX_NEWS) {
+        System.out.printf("%d news received (max: %d), cancelling subscription\n", newsReceived, MAX_NEWS);
+        subscription.cancel();
+        return;
+      }
+
       subscription.request(1);
 
     }
@@ -67,13 +73,18 @@ public class Main {
 
 
   public static void main(String[] args) {
-    try (NewsPublisher newsPublisher = new NewsPublisher()) {
+    try (SubmissionPublisher<News> newsPublisher   = new SubmissionPublisher()) {
 
       NewsSubscriber newsSubscriber = new NewsSubscriber();
       newsPublisher.subscribe(newsSubscriber);
 
       List.of(News.create("Important news"), News.create("Some other news"),
-          News.create("And news, news, newws")).forEach(newsPublisher::submit);
+          News.create("And news, news, news")).forEach(newsPublisher::submit);
+
+      while(newsPublisher.hasSubscribers()){
+        // wait
+      }
+      System.out.println("no more news subscribers left, closing publisher..");
     }
   }
 }
